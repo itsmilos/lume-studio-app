339e6120d70c8cb5e15dcd54c8aceeb20a45b90e
skem-skem <lazenda001@gmail.com>
2026-06-03 13:19:59 +0200

    fixed version

--- app/api/bookings/[id]/route.ts
+++ app/api/bookings/[id]/route.ts
@@ -1,35 +1,32 @@
-import { connectionDB } from "@/lib/db";
-import { NextResponse } from "next/server";
-
 export const dynamic = "force-dynamic";
 
-export async function DELETE(
-    request: Request,
-    { params }: { params: { id: string } }
-) {
+import { connectionDB } from '@/lib/db'
+import { NextResponse } from 'next/server';
+import mongoose from 'mongoose';
+
+export async function GET() {
     await connectionDB();
-
-    const Booking = (await import('@/lib/models/Booking')).default;
-
-    const { id } = params;
-
-    await Booking.deleteOne({ _id: id });
-
-    return NextResponse.json({ message: 'Booking deleted!' });
+    const Review = (await import('@/lib/models/Review')).default;
+    const data = await Review.find().populate('service');
+    return NextResponse.json(data);
 }
 
-export async function PATCH(
-    request: Request,
-    { params }: { params: { id: string } }
-) {
-    await connectionDB();
+export async function POST(request: Request) {
+    try {
+        const data = await request.json();
 
-    const Booking = (await import('@/lib/models/Booking')).default;
+        if (!data.firstName || !data.lastName || !data.email || !data.text || !data.service) {
+            return NextResponse.json({ message: 'Missing fields' }, { status: 400 })
+        }
 
-    const { id } = params;
-    const { status } = await request.json();
-
-    await Booking.findOneAndUpdate({ _id: id }, { status });
-
-    return NextResponse.json({ message: 'Succesfully changed status!' });
+        await connectionDB();
+        const Review = (await import('@/lib/models/Review')).default;
+        await Review.create({
+            ...data,
+            service: new mongoose.Types.ObjectId(data.service)
+        })
+        return NextResponse.json({ message: 'Success!', received: data }, { status: 201 });
+    } catch (error) {
+        return NextResponse.json({ message: 'Error' }, { status: 500 });
+    }
 }
--- app/api/services/route.ts
+++ app/api/services/route.ts
@@ -2,20 +2,18 @@ export const dynamic = "force-dynamic";
 
 import { connectionDB } from '@/lib/db'
 import { NextResponse } from 'next/server';
-import mongoose from 'mongoose';
-const Service = mongoose.models.Service || mongoose.model('Service');
 
 export async function GET() {
     await connectionDB();
+    const Service = (await import('@/lib/models/Service')).default;
     const data = await Service.find();
     return NextResponse.json(data);
 }
 
 export async function POST(request: Request) {
     const data = await request.json();
-
     await connectionDB();
+    const Service = (await import('@/lib/models/Service')).default;
     await Service.create(data);
-
     return NextResponse.json({ message: 'Success!', received: data }, { status: 201 });
 }
