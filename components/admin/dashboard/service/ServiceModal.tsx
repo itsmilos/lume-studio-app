602d24e9a138edc139d4d7a1b97a2ca0796f546f
skem-skem <lazenda001@gmail.com>
2026-06-04 13:59:50 +0200

    fixed paths

--- components/admin/dashboard/service/ServiceModal.tsx
+++ components/admin/dashboard/service/ServiceModal.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from "react";
-import AddServiceForm from "./dashboard/service/AddServiceForm";
+import AddServiceForm from "./AddServiceForm";
 import { X } from "lucide-react";
 
 export default function ServiceModal() {
